f4f2b5cb43112a41e5f1cd878e38c559a4a865db
RasmusSjo <rasmus.sjoberg15@gmail.com>
2026-03-07 20:50:38 +0100

    feat: add handling of DOM propose and ack messages
    - create_dom_propose that returns a DOM proposal
    - handle_dom_propose that inserts the proposal in the eb/lb

---
+++ omnipaxos/src/dom/mod.rs
@@ -0,0 +1,136 @@
+use std::cmp::max;
+use crate::dom::buffer::{EarlyBuffer, EarlyBufferInsertError, LateBuffer};
+use crate::dom::owd_estimator::{OutgoingOwdTracker, OwdEstimator, OwdSample};
+use crate::messages::sequence_paxos::{DomAck, DomPropose, EntryId, ClockTimestamp};
+use crate::sequence_paxos::Role;
+use crate::storage::Entry;
+use crate::util::{NodeId, PhysicalClock};
+
+mod buffer;
+mod owd_estimator;
+pub(crate) use owd_estimator::{OwdEstimatorConfig, EstimatorStrategy};
+
+const ONE_MICRO_SECOND: i64 = 1000;
+
+/// The Deadline-Ordered-Multicast abstraction.
+pub(crate) struct Dom<'a, T, C>
+where
+    T: Entry,
+    C: PhysicalClock,
+{
+    pid: NodeId,
+    clock: &'a C,
+    incoming_owd_estimates: OwdEstimator,
+    outgoing_owd_estimates: OutgoingOwdTracker,
+    eb: EarlyBuffer<T>,
+    lb: LateBuffer<T>,
+}
+
+impl<'a, T, C> Dom<'a, T, C>
+where
+    T: Entry,
+    C: PhysicalClock,
+{
+    /// Creates a new instance of the DOM abstraction.
+    pub(crate) fn new(config: OwdEstimatorConfig, clock: &'a C, pid: NodeId) -> Self {
+        Self {
+            pid,
+            clock,
+            incoming_owd_estimates: OwdEstimator::with_config(config),
+            outgoing_owd_estimates: OutgoingOwdTracker::new(config.max_owd()),
+            eb: EarlyBuffer::new(),
+            lb: LateBuffer::new(),
+        }
+    }
+
+    /// Creates a new DOM proposal for the given entry.
+    pub(crate) fn create_dom_propose(&self, entry: T, entry_id: EntryId) -> DomPropose<T> {
+        let sent_time: ClockTimestamp = self.clock.get_time_with_uncertainty();
+
+        DomPropose {
+            entry_id,
+            sender: self.pid,
+            sent_time,
+            deadline: self.get_deadline(sent_time.0),
+            entry,
+        }
+    }
+    
+    fn get_deadline(&self, time: i64) -> i64 {
+        // TODO do we need this to be more pessimistic? E.g. deadline = OWD + uncertainty
+        time + self.outgoing_owd_estimates.get_max_owd()
+    }
+
+    /// Returns all proposals in the early buffer with a deadline that is
+    /// before the current time.
+    /// TODO Should we make the return type Vec<T> instead?
+    pub(crate) fn release_ready(&mut self) -> Vec<DomPropose<T>> {
+        let now = self.clock.get_time();
+        let mut proposals = Vec::new();
+
+        while let Some(prop) = self.eb.pop_ready(now) {
+            proposals.push(prop);
+        }
+
+        proposals
+    }
+
+    /// Handles a DOM proposal for the given role. Returns an ack with the
+    /// estimated one-way delay of the proposal to be returned to the sender
+    pub(crate) fn handle_dom_propose(
+        &mut self,
+        prop: DomPropose<T>,
+        role: Role,
+    ) -> DomAck {
+        let received_time = self.clock.get_time_with_uncertainty();
+        let owd_sample = OwdSample::new(
+            prop.sent_time.0,
+            received_time.0,
+            prop.sent_time.1,
+            received_time.1,
+        );
+        
+        self.incoming_owd_estimates.update(prop.sender, owd_sample);
+        
+        let ack = DomAck {
+            sender: self.pid,
+            estimated_owd: self.incoming_owd_estimates.estimate_for(prop.sender),
+        };
+
+        match self.eb.insert(prop) {
+            Ok(_) => { },
+            Err(EarlyBufferInsertError::Late(prop)) => {
+                self.lb.insert(prop.clone());
+                if role == Role::Leader {
+                    let prop = self.extend_deadline(prop);
+                    self.eb.insert(prop).expect("proposal cannot be late");
+                }
+            }
+        };
+
+        ack
+    }
+
+    fn extend_deadline(&self, prop: DomPropose<T>) -> DomPropose<T> {
+        let last_deadline = self.eb.last_released_deadline();
+        let current_time = self.clock.get_time();
+        
+        let new_deadline = match last_deadline {
+            Some(last) => max(current_time, last + ONE_MICRO_SECOND),
+            None => current_time,
+        };
+
+        DomPropose {
+            entry_id: prop.entry_id,
+            sender: prop.sender,
+            sent_time: prop.sent_time,
+            deadline: new_deadline,
+            entry: prop.entry,
+        }
+    }
+
+    /// Handles a DOM ack.
+    pub(crate) fn handle_dom_ack(&mut self, ack: DomAck) {
+        self.outgoing_owd_estimates.update(ack.sender, ack.estimated_owd);
+    }
+}
